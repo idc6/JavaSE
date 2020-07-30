3f4d64966feeb23e7079592df4f9fe48d3434965
you <you@qq.com>
2020-07-30 10:55:03 +0800

    v1.0

---
+++ Test1.java
@@ -0,0 +1,22 @@
+package com.neuedu.test.chapter3;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// 我不管，我最帅，我是你的小可爱
+		int i = 55;
+        if (i <= 100 && i >= 90) {
+            System.out.println("成绩是A");
+        } else if (i >= 80) {
+            System.out.println("成绩是B");
+        } else if (i >= 70) {
+            System.out.println("成绩是C");
+        } else if (i >= 60) {
+            System.out.println("成绩是D");
+        } else {
+            System.out.println("成绩是E");
+        }
+
+	}
+
+}
